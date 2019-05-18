8349f0e15d4d2a27a64fbaf5d3b21213b0e5de99
Pedro Wigderowitz <pedrohm706@hotmail.com>
2019-05-18 17:12:11 +0100

    updating setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name="fightin_words",
-      version="1.0.0",
+      version="1.0.1",
       description="Implementation of Monroe et al., 2008.",
       url="https://github.com/Wigder/fightin_words",
       author="Pedro Wigderowitz/Jack Hessel",
       author_email="pedrohm706@hotmail.com",
-      python_requires=">=3.6.0",
+      python_requires=">=3",
       license="MIT",
       packages=["fightin_words"],
       install_requires=[
